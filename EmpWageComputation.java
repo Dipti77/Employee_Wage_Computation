ec4f31d104169c8d63047d51cc181a98a6ebbd71
Dipti77 <diptimhandlekar123@gmail.com>
2023-04-29 00:22:48 +0530

    Added: calculation of monthly wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -7,29 +7,33 @@ public class EmpWageComputation {
 		final int IS_PART_TIME = 1;
 		final int IS_Full_TIME = 2;
 		final int EMP_WAGE_PER_HR = 20;
+		final int WORKING_DAYS = 20;
 		
 		int empHrs = 0;
+		int totalWage = 0;
 		int empWage = 0;
 		
-		int empCheck = (int) ((Math.random()*10) % 3);
-		
-		switch (empCheck)
+		for (int day=1; day<=WORKING_DAYS; day++)
 		{
-		case IS_Full_TIME:
-			System.out.println("Employee is Present Full time");
-			empHrs = 8;
-			break;
-		case IS_PART_TIME:
-			System.out.println("Employee is Present Part time");
-			empHrs = 4;
-			break;
-		default:
-			System.out.println("Employee is Absent");
-			empHrs = 0;
-		}
+			int empCheck = (int) ((Math.random()*10) % 3);
+			switch (empCheck)
+			{
+				case IS_Full_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
 		
-		empWage = EMP_WAGE_PER_HR * empHrs;
-		System.out.println("Daily employee wage: " + empWage);
+			empWage = EMP_WAGE_PER_HR * empHrs;
+			System.out.println("Day: " + day + " Emp wage is: " + empWage);
+			totalWage = totalWage + empWage;
+		}
+	
+		System.out.println("Employee wage for a month : " + totalWage);
 		
 	}
 }
