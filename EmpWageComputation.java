1bd0705897403dfc872b97d6fc18076425962855
Dipti77 <diptimhandlekar123@gmail.com>
2023-04-28 23:53:54 +0530

    Added: Solving using switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,24 +4,26 @@ public class EmpWageComputation {
 
 	public static void main(String[] args) {
 		
-		int IS_PART_TIME = 1;
-		int IS_Full_TIME = 2;
-		int EMP_WAGE_PER_HR = 20;
+		final int IS_PART_TIME = 1;
+		final int IS_Full_TIME = 2;
+		final int EMP_WAGE_PER_HR = 20;
 		
 		int empHrs = 0;
 		int empWage = 0;
 		
 		int empCheck = (int) ((Math.random()*10) % 3);
 		
-		if (empCheck == IS_Full_TIME) {
+		switch (empCheck)
+		{
+		case IS_Full_TIME:
 			System.out.println("Employee is Present Full time");
 			empHrs = 8;
-		}
-		else if(empCheck == IS_PART_TIME) {
+			break;
+		case IS_PART_TIME:
 			System.out.println("Employee is Present Part time");
 			empHrs = 4;
-		}
-		else {
+			break;
+		default:
 			System.out.println("Employee is Absent");
 			empHrs = 0;
 		}
