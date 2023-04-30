3b1006e8c332013a8b75ff66b6764fed1d20e3fd
Dipti77 <diptimhandlekar123@gmail.com>
2023-04-30 16:29:47 +0530

    Added: class method to compute emp wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,13 +2,13 @@ package com.bridgelabz;
 
 public class EmpWageComputation {
 
-	public static void main(String[] args) {
-		
-		final int IS_PART_TIME = 1;
-		final int IS_Full_TIME = 2;
-		final int EMP_WAGE_PER_HR = 20;
-		final int WORKING_DAYS = 20;
-		final int MAX_WORKING_HRS = 100;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_Full_TIME = 2;
+	public static final int EMP_WAGE_PER_HR = 20;
+	public static final int WORKING_DAYS = 20;
+	public static final int MAX_WORKING_HRS = 100;
+
+	public void computeEmpWage(){
 		
 		int empHrs = 0;
 		int totalEmpHrs = 0;
@@ -16,11 +16,11 @@ public class EmpWageComputation {
 		int empWage = 0;
 		int totalEmpWage = 0;
 		
-		
 		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < WORKING_DAYS)
 		{
 			totalWorkingDays++;
 			int empCheck = (int) ((Math.random()*10) % 3);
+			
 			switch (empCheck)
 			{
 				case IS_Full_TIME:
@@ -40,8 +40,14 @@ public class EmpWageComputation {
 			System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs + " Emp wage: " + empWage);
 			
 		}
-
+		
 		System.out.println("Total Employee wage : " + totalEmpWage);
+	}
+	
+	public static void main(String[] args) {
+		
+		EmpWageComputation ewc = new EmpWageComputation();
+		ewc.computeEmpWage();
 		
 	}
 }
